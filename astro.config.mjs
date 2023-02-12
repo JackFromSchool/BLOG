3d9471042199c8ddb3fe83b04df539e1e990dd55
BuildTools <jack.p.saenz@gmail.com>
2023-02-12 13:46:56 -0600

    Home Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [mdx(), react()]
+});
