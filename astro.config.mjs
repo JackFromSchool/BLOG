0fd42138f82ead2ab9b4c47324fa99c254e2d741
BuildTools <jack.p.saenz@gmail.com>
2023-02-13 23:59:25 -0600

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  i tegrations: [mdx(), react()],
+  integrations: [mdx(), react()],
   output: "server",
   adapter: vercel()
 });
